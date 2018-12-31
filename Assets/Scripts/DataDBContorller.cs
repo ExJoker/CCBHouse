297cae18716e7b53696af9a9d971390eca780a0a
ExJoker <maojialex@163.com>
2018-12-31 18:55:13 +0800

    Init

---
+++ Assets/Scripts/DataDBContorller.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 需要引用的DLL文件：
+/// Mysql.Data.dll
+/// 在unity中需要：
+/// l18n.cjk.dll
+/// l18n.dll
+/// system.data.dll
+/// system.drawing.dll
+/// </summary>
+
+public class DataDBContorller: MonoBehaviour
+{
+	//远程连接
+	//  string connectionString= "User ID = ; Password =.; Host =; Port =3306;Database = ;Charset = ";
+	//本地连接
+	public static     string connectionString= "User ID =root ; Password =888888 ; Host =localhost ; Port = 3306;Database =ccbhouse2 ;Charset =utf8";
+
+	public static MySqlConnection dbConnection;
+
+	static void Main(string[] args)
+	{
+	}
+
+	 
+        
+	//打开数据库链接
+	public static void OpenSqlConnection(string connectionString)
+	{
+		dbConnection = new MySqlConnection(connectionString);
+		dbConnection.Open();
+	}
+
+	//关闭数据库连接
+	public static void CloseConnection()
+	{
+		if (dbConnection != null)
+		{
+			dbConnection.Close();
+			dbConnection.Dispose();
+			dbConnection = null;
+		}
+	}
+
+	//保存数据
+	public static  DataSet GetDataSet(string sqlString)
+	{
+		DataSet ds = new DataSet();
+		try
+		{
+			//用于检索和保存数据
+			//Fill(填充)能改变DataSet中的数据以便于数据源中数据匹配
+			//Update(更新)能改变数据源中的数据以便于DataSet中的数据匹配
+              
+			MySqlDataAdapter da = new MySqlDataAdapter(sqlString, dbConnection);
+			da.Fill(ds);
+
+
+
+		}
+		catch (Exception ee)
+		{
+			throw new Exception("SQL:" + sqlString + "\n" + ee.Message.ToString());
+		}
+		return ds;
+
+
+
+	}
+
+	//增 insert
+	static void Add()
+	{
+		OpenSqlConnection(connectionString);
+		string sqlstring= "insert into userinformation(name,password,tel) values();";//保证sql语句的正确性
+		GetDataSet(sqlstring);
+		CloseConnection();
+	}
+
+	//删 delete
+	static void Delete()
+	{
+		OpenSqlConnection(connectionString);
+		string sqlstring = "delete from 表名;";
+		GetDataSet(sqlstring);
+		CloseConnection();
+	}
+
+	//改 update
+	public static void Update(string sqlstring)
+	{
+		OpenSqlConnection(connectionString);
+		GetDataSet(sqlstring);
+		CloseConnection();
+
+	}
+
+	//查 select
+	public static List<string[]> Select(String sql)
+	{
+		
+		List<string[]> list =new List<string[]>();
+		String[] temp;
+		OpenSqlConnection(connectionString);
+		MySqlCommand mysqlcommand = new MySqlCommand(sql, dbConnection);
+		MySqlDataReader reader = mysqlcommand.ExecuteReader();
+		temp=new string[reader.FieldCount];
+		try
+		{
+			while (reader.Read())
+			{
+				if (reader.HasRows)
+				{
+					for (int i = 0; i < reader.FieldCount; i++)
+					{
+							try{
+								temp[i]= reader.GetString(i);
+							}
+							catch(Exception)
+							{
+								temp[i]= "";
+							}
+							
+					}
+					list.Add(temp);
+					temp=new string[reader.FieldCount];
+				}
+			}
+			return list;
+		}
+		catch (Exception e)
+		{
+			Debug.Log(e.ToString());
+			return null;
+		}
+		finally
+		{
+			reader.Close();
+			CloseConnection();
+		}
+
+	}
+	static void resultToString(List<string[]> list)
+	{
+		foreach(string[] s in list)
+		{
+			for(int i=0 ;i<s.Length ; i++)
+			{
+				Debug.Log(list.IndexOf(s)+":"+s[i]);
+			}
+		}
+	}
+
+	}
+
+
+	
---
+++ Assets/Scripts/TestSQL.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSQL : MonoBehaviour {
+
+    public Text textStr;
+
+    public string[] RecStr;
+    public GameObject rawImg;
+
+	// Use this for initialization
+	void Start () {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Select();
+        }	
+	}
+
+
+    public void TestSql()
+    {
+        DataDBContorller.OpenSqlConnection(DataDBContorller.connectionString);
+    }
+
+    public void CloseSql()
+    {
+        DataDBContorller.CloseConnection();
+    }
+
+    public void Select()
+    {
+        RecStr = DataDBContorller.Select("SELECT * FROM `Data`;")[0] as string[];
+        //for (int i = 0; i < RecStr.Length; i++)
+        //{
+        //    RecStr[i].Trim();
+        //}
+        textStr.text = RecStr[0] + "\n" + RecStr[1].Trim() + "\n" +
+            RecStr[2] + "\n" + RecStr[4] + "元/月起\n" + RecStr[5] +"㎡"
+            ;
+        // Debug.Log(DataDBContorller.Select("SELECT * FROM `Data`;")[0]as string[]);
+        StartCoroutine(LoadTextureToMainTexture());
+    }
+
+    IEnumerator LoadTextureToMainTexture()
+    {
+        WWW www = new WWW(RecStr[3]);
+        yield return www;
+        rawImg.GetComponent<RawImage>().texture = www.texture;
+    }
+}
